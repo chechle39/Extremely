7ebfc4911338a4f9a2a42e26d977154e32bb4113
unknown <chechle3979@gmail.com>
2020-03-16 18:05:28 +0700

    x

---
+++ XBOOK/XBOOK.Web/ClientApp/src/app/pages/journalentries/validate/validateinputacc.ts
@@ -0,0 +1,22 @@
+import { Directive, Input, Inject } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl} from '@angular/forms';
+import { MessageService } from '../../../coreapp/services/message.service';
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[validateInputAccDirective]',
+  providers: [{ provide: NG_VALIDATORS, useExisting: validateInputAccDirective, multi: true }],
+})
+
+// tslint:disable-next-line:class-name
+export class validateInputAccDirective implements Validator {
+  @Input() rowItem: any;
+  constructor(private message: MessageService) {
+
+  }
+  validate(control: AbstractControl): { [key: string]: any } {
+    if (control.value.accountNumber !== undefined || control.value === '') {
+      return null;
+    }
+    return { accNumber: true };
+  }
+}
